ac84e7e51dd6113629d40b0e627dc78b4c78b458
Adayane Coutinho <alucas@supriservice.com.br>
2016-07-21 10:30:11 -0300

    subindo

---
+++ config/config_db.php
@@ -0,0 +1,13 @@
+<?php
+ class DB extends DBmysql {
+                
+ var $dbhost = '127.0.0.1';
+                
+ var $dbuser 	= 'root';
+                
+ var $dbpassword= 'supri2011';
+                
+ var $dbdefault	= 'glpi_supri';
+                
+ } 
+?>
